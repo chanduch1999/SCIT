4bdc4bd2f6252256302ca9e2da8723bd999909fa
chandu <32795376+chanduch1999@users.noreply.github.com>
2018-10-29 22:02:45 +0530

    Update normal.py

--- normal.py
+++ normal.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 for i in range(3):
-    x,fr=0,0.3
-    _ = np.random.normal(x,fr,1000)
+    # Generates a random distribution,location : mean of the distribution, scale : width of the distribution,size : output shape
+    _ = np.random.normal(location=0,scale=0.3,size=1000)
+    # Lables and title for Histogram
     plt.xlabel('normal distrbution')
     plt.ylabel('probability')
     plt.title('Histogram for Random Normal Distribution')
+    # Generates the Histogram for above data
     bins = plt.hist(_,20,density=True,color='g')
+    # Displays the Histogram
     plt.show()
-
 
