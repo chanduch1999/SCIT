515397318f2ff3eca89d5758301670143971ddde
chandu <32795376+chanduch1999@users.noreply.github.com>
2018-10-29 22:02:05 +0530

    Update poisson.py

--- poisson.py
+++ poisson.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 for i in range(3):
-    c=12
-    _ = np.random.poisson(c,1000)
+    ex=12
+    # Generates random poisson distribution 
+    _ = np.random.poisson(ex,size=1000)
+    # labels and title for histogram
     plt.xlabel('poisson distributions')
-    plt.ylabel('Probability')
+    plt.ylabel('Probability') 
     plt.title('Histogram for Random Poisson Distribution')
+    # Generates a histogram using the above data
     bins=plt.hist(_,20,density=True,color='g')
+    # Displays the histogram
     plt.show()
-
